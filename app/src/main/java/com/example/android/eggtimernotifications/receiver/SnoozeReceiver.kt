308ef855c2d95cda30098cc7459a5ed8f29b9f6a
JamesDLechu <jamesdlechu@gmail.com>
2023-03-04 20:10:50 -0600

    Update: Action added to notification
    - Postpone behavior implemented

--- app/src/main/java/com/example/android/eggtimernotifications/receiver/SnoozeReceiver.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/receiver/SnoozeReceiver.kt
@@ -25,7 +25,9 @@ import android.content.Intent
 import android.os.SystemClock
 import android.text.format.DateUtils
 import androidx.core.app.AlarmManagerCompat
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.cancelNotifications
 
 class SnoozeReceiver: BroadcastReceiver() {
     private val REQUEST_CODE = 0
@@ -47,6 +49,12 @@ class SnoozeReceiver: BroadcastReceiver() {
             triggerTime,
             notifyPendingIntent
         )
+
+        val notificationManager = ContextCompat.getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+        notificationManager.cancelNotifications()
     }
 
 }
